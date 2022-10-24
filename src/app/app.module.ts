98dd14613850c8d1e621a9a79bd5de3ee7b521d6
Iñaki Vladimir Moix <iniakivmoix@gmail.com>
2022-10-24 17:03:36 -0300

    CSS, buttons, menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent } from "./AppComponent";
 import { HomeComponent } from './components/home/home.component';
 import { WorkExperienceComponent } from './components/work-experience/work-experience.component';
 import { ContactComponent } from './components/contact/contact.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     WorkExperienceComponent,
-    ContactComponent
+    ContactComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
